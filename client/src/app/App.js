fc44fbc78478e7cec4ea846c18e825aec0d83e81
Fernan de Dios <info@mediagramstudios.com>
2018-06-18 12:10:10 +0800

    added initial footer component

--- client/src/app/App.js
+++ client/src/app/App.js
@@ -2,15 +2,19 @@ import '../index.css';
 import React, { Component } from 'react';
 
 import Header from './Header';
+import Footer from './Footer';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Header />
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="main-content">
+          <p className="App-intro">
+            To get started, edit <code>src/App.js</code> and save to reload.
+          </p>
+        </div>
+        <Footer />
       </div>
     );
   }
---
+++ client/src/app/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <footer className="footer">
+            <div className="container">
+                <div className="content has-text-centered">
+                    <p>&copy; Easy Board. Open Source under MIT License.</p>
+                </div>
+            </div>
+        </footer>
+    );
+};
+
+export default Footer;
