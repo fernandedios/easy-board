5a8b1f126a5f88a9d5419793197e6751562e787c
Fernan de Dios <info@mediagramstudios.com>
2018-06-28 16:22:56 +0800

    added apollo configuration to client

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { ApolloProvider } from 'react-apollo';
+import { ApolloClient, HttpLink, InMemoryCache } from 'apollo-boost';
+
 import App from './app/App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const client = new ApolloClient({
+    link: new HttpLink({ uri: process.env.REACT_APP_API }),
+    cache: new InMemoryCache()
+});
+
+ReactDOM.render(
+    <ApolloProvider client={client}>
+        <App />
+    </ApolloProvider>, 
+    document.getElementById('root')
+);
 registerServiceWorker();
