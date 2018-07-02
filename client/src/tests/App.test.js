4ea0e206ddc15eea97e55ba3abfbd7b71fe6953c
Fernan de Dios <info@mediagramstudios.com>
2018-07-02 13:20:41 +0800

    additional app tests

--- client/src/tests/App.test.js
+++ client/src/tests/App.test.js
@@ -5,3 +5,13 @@ import App from '../app/App';
 it('renders without crashing', () => {
   shallow(<App />);
 });
+
+it('renders a header component', () => {
+  const output = mount(<App />);
+  expect(output.find('nav.navbar').length).toEqual(1);
+});
+
+it('renders a footer component', () => {
+  const output = mount(<App />);
+  expect(output.find('footer.footer').length).toEqual(1);
+});
