3597bafbf59ac3fcd64a8b50c46d37a69061c9c2
Fernan de Dios <info@mediagramstudios.com>
2018-07-18 15:46:28 +0800

    added initial redux setup

--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
 import { HashRouter } from 'react-router-dom'
+import reduxThunk from 'redux-thunk';
 
 import App from './app/App';
 import registerServiceWorker from './registerServiceWorker';
+import reducers from './login/LoginReducer';
+
+const store = createStore(reducers, {}, applyMiddleware(reduxThunk));
 
 ReactDOM.render(
-    <HashRouter><App /></HashRouter>, 
+    <Provider store={store}>
+        <HashRouter><App /></HashRouter>
+    </Provider>, 
     document.getElementById('root')
 );
 
