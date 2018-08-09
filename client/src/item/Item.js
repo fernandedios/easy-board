53f6737ef1f0ad9da06f71866e0729af8d2082ab
Fernan de Dios <info@mediagramstudios.com>
2018-08-09 19:37:07 +0800

    added item boilerplate

--- client/src/item/Item.js
+++ client/src/item/Item.js
@@ -2,7 +2,14 @@ import React, { Component } from 'react';
 
 class Item extends Component {
     render() {
-        return <div></div>
+        return (
+            <div className="item">
+                First Task!
+                <a href="#"><i className="fa fa-fw fa-close hover-icon"> </i></a>
+                <a href="#"><i className="fa fa-fw fa-pencil hover-icon"></i></a>
+                <a href="#"><i className="fa fa-fw fa-ellipsis-v hover-icon"></i></a>
+            </div>
+        );
     }
 }
 
