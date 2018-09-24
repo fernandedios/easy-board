3abcab2e137743d03b31a7365067e589456da14e
Fernan de Dios <info@mediagramstudios.com>
2018-09-24 11:51:26 +0800

    typo fix removed href

--- client/src/app/App.js
+++ client/src/app/App.js
@@ -10,7 +10,7 @@ import Signup from '../signup/Signup';
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div className="app">
         <Header />
         <div className="main-content">
           <Switch>
