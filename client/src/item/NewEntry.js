88798340e62cf58150a3b855ac98183ad346d25d
Fernan de Dios <info@mediagramstudios.com>
2018-09-27 16:02:19 +0800

    added control to NewEntry component

--- client/src/item/NewEntry.js
+++ client/src/item/NewEntry.js
@@ -1,18 +1,34 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const NewEntry = ({ placeholder, type }) => {
-    return (
-        <div class="ui-item-entry field has-addons">
-            <div class="control is-expanded">
-                <input placeholder={placeholder} class="input" />
+class NewEntry extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            entry: ''
+        };
+    }
+
+    handleEvent(e) {
+        e.preventDefault();
+        this.setState({ [e.target.name]: e.target.value });
+    }
+
+    render() {
+        const { placeholder, type } = this.props;
+        return (
+            <div class="ui-item-entry field has-addons">
+                <div class="control is-expanded">
+                    <input name="entry" onChange={this.handleEvent} placeholder={placeholder} value={this.state.entry} class="input" />
+                </div>
+                <div class="control">
+                    <button type="submit" class="button is-primary">
+                        <span class="icon is-small"><i class="fas fa-angle-right"></i></span>
+                    </button>
+                </div>
             </div>
-            <div class="control">
-                <button type="submit" class="button is-primary">
-                    <span class="icon is-small"><i class="fas fa-angle-right"></i></span>
-                </button>
-            </div>
-        </div>
-    );
+        );
+    }
 }
 
 export default NewEntry;
