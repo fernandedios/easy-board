966790e39f81506646efea23f32c33702c291ede
Fernan de Dios <info@mediagramstudios.com>
2018-10-05 16:50:17 +0800

    modal component improvement

--- client/src/common/Modal.js
+++ client/src/common/Modal.js
@@ -3,12 +3,24 @@ import Button from './Button';
 
 class Modal extends Component {
     render() {
-        const { name, content } = this.props;
+        const { name, title, content } = this.props;
 
         return (
             <div id={name} className="modal">
                 <div className="modal-background"></div>
-                <div className="modal-content">{content}</div>
+                <div className="modal-card">
+                    <header className="modal-card-head">
+                        <p className="modal-card-title">{title}</p>
+                        <Button classes="delete" aria-label="close" />
+                    </header>
+                    <section class="modal-card-body">
+                        {content}
+                    </section>
+                    <footer className="modal-card-foot">
+                        <Button classes="is-success" name="modal-btn" label="Save" />
+                        <Button name="modal-btn" label="Cancel" />
+                    </footer>
+                </div>
                 <Button classes="modal-close is-large" name="modal-btn" aria-label="close" />
             </div>
         );
