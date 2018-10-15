e0bc8f5eb3dcf0bb23398f34e4488c52ce849cd4
Fernan de Dios <info@mediagramstudios.com>
2018-10-15 09:39:44 +0800

    test refactor

--- client/src/tests/Button.test.js
+++ client/src/tests/Button.test.js
@@ -5,24 +5,27 @@ import ReactTestRenderer from 'react-test-renderer';
 import Button from '../common/Button';
 
 describe('<Button />', () => {
+    let output;
+    beforeEach(() => {
+        output = shallow(<Button name="test" classes="other-class" />);
+    });
+
     it('should be defined', () => {
         expect(Button).toBeDefined();
     });
 
     it('should render the button name correctly', () => {
-        const output = shallow(<Button name="test" />);
         expect(typeof(output.prop('name'))).toBe('string');
         expect(output.prop('name'))
     });
 
     it('should have the correct classNames', () => {
-        const output = shallow(<Button classes="other-class" />);
         expect(output.find('button').hasClass('button')).toEqual(true);
         expect(output.find('button').hasClass('other-class')).toEqual(true);
     });
 
     it('should render correctly', () => {
-        const output = ReactTestRenderer.create(<Button label="test" />).toJSON();
-        expect(output).toMatchSnapshot();
+        const render = ReactTestRenderer.create(<Button label="test" />).toJSON();
+        expect(render).toMatchSnapshot();
     });
 });
--- client/src/tests/Footer.test.js
+++ client/src/tests/Footer.test.js
@@ -1,26 +1,29 @@
 import React from 'react';
-import { shallow, mount } from 'enzyme';
+import { mount } from 'enzyme';
 import ReactTestRenderer from 'react-test-renderer';
 
 import Footer from '../app/Footer';
 
 describe('<Footer />', () => {
+    let output;
+    beforeEach(() => {
+        output = mount(<Footer />);
+    });
+
     it('should be defined', () => {
         expect(Footer).toBeDefined();
     });
     
     it('should have correct classNames', () => {
-        const output = shallow(<Footer />);
         expect(output.find('footer').hasClass('footer')).toEqual(true);
     });
 
     it('should render footer notice', () => {
-        const output = mount(<Footer />);
         expect(output.find('.content').text()).toEqual('© Easy Board. Open Source under MIT License.');
     });
 
     it('should render correctly', () => {
-        const output = ReactTestRenderer.create(<Footer />).toJSON();
-        expect(output).toMatchSnapshot();
+        const render = ReactTestRenderer.create(<Footer />).toJSON();
+        expect(render).toMatchSnapshot();
     });
 });
