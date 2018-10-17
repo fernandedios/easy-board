42bf9f27e786c5b3cbe95f9a78f4c2ea50f5305f
Fernan de Dios <info@mediagramstudios.com>
2018-10-17 17:07:22 +0800

    refactor header tests

--- client/src/tests/Header.test.js
+++ client/src/tests/Header.test.js
@@ -1,26 +1,29 @@
 import React from 'react';
-import { shallow, mount } from 'enzyme';
+import { mount } from 'enzyme';
 import ReactTestRenderer from 'react-test-renderer';
 
 import { Header } from '../app/Header';
 
 describe('<Header />', () => {
+    let output;
+    beforeEach(() => {
+        output = mount(<Header />);
+    });
+
     it('should be defined', () => {
         expect(Header).toBeDefined();
     });
     
     it('should have correct classNames', () => {
-        const output = shallow(<Header />);
         expect(output.find('nav').hasClass('navbar')).toEqual(true);
     });
 
     it('should render menu', () => {
-        const output = mount(<Header />);
         expect(output.find('.navbar-end').children()).toHaveLength(4);
     });
 
     it('should render correctly', () => {
-        const output = ReactTestRenderer.create(<Header />).toJSON();
-        expect(output).toMatchSnapshot();
+        const render = ReactTestRenderer.create(<Header />).toJSON();
+        expect(render).toMatchSnapshot();
     });
 });
