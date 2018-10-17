e992c550ef96ab02d5da00be3dd82e8e60a1e351
Fernan de Dios <info@mediagramstudios.com>
2018-10-17 17:07:42 +0800

    refactor text input tests

--- client/src/tests/TextInput.test.js
+++ client/src/tests/TextInput.test.js
@@ -1,27 +1,30 @@
 import React from 'react';
-import { shallow, mount } from 'enzyme';
+import { mount } from 'enzyme';
 import ReactTestRenderer from 'react-test-renderer';
 
 import TextInput from '../common/TextInput';
 
 describe('<TextInput />', () => {
+    let output;
+    beforeEach(() => {
+        output = mount(<TextInput type="text" name="test" placeholder="test placeholder" />);
+    });
+
     it('should be defined', () => {
         expect(TextInput).toBeDefined();
     });
 
     it('should have the correct classNames', () => {
-        const output = shallow(<TextInput name="test" />);
         expect(output.find('input').hasClass('input')).toEqual(true);
     });
 
     it('should render input field correctly', () => {
-        const output = mount(<TextInput type="text" name="test" placeholder="test placeholder" />);
         expect(output.find('input').prop('placeholder')).toEqual('test placeholder');
         expect(output.find('input').prop('type')).toEqual('text');
     });
 
     it('should render correctly', () => {
-        const output = ReactTestRenderer.create(<TextInput name="test" />).toJSON();
-        expect(output).toMatchSnapshot();
+        const render = ReactTestRenderer.create(<TextInput name="test" />).toJSON();
+        expect(render).toMatchSnapshot();
     });
 });
